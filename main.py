8effbec2b996170cbb25f85bcc166f1769388819
hugermuger <tobias@quasiso.de>
2025-10-02 17:25:07 +0200

    sorted list added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +8,16 @@ def get_book_text(filepath):
 def main():
     filepath = "books/frankenstein.txt"
     num_words = get_num_words(get_book_text(filepath))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(get_num_characters(get_book_text(filepath)))
+    print("--------- Character Count -------")
+    num_dic = get_num_characters(get_book_text(filepath))
+    sorted_num_dic = sorted_list(num_dic)
+    for i in sorted_num_dic:
+        if i["name"].isalpha() and i["name"] != None:
+            print(f"{i["name"]}: {i["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,22 @@
+def sort_on(items):
+    return items["num"]
+
 def get_num_words(book_text):
     return len(book_text.split())
 
 def get_num_characters(book_text):
     charakter_dic = {}
     for c in book_text:
-        if c.lower() in charakter_dic:
-            charakter_dic[c.lower()] += 1
+        c = c.lower()
+        if c in charakter_dic:
+            charakter_dic[c] += 1
         else:
-            charakter_dic[c.lower()] = 1
+            charakter_dic[c] = 1
     return charakter_dic
+
+def sorted_list(input_dict):
+    new_list = []
+    for i in input_dict:
+        new_list.append({"name": i, "num": input_dict[i]})
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
