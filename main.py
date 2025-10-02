d1f0836f060800d18db0d73c58057e58f3b70e18
hugermuger <tobias@quasiso.de>
2025-10-02 16:20:51 +0200

    add charakter count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,6 @@ def main():
     filepath = "books/frankenstein.txt"
     num_words = get_num_words(get_book_text(filepath))
     print(f"Found {num_words} total words")
+    print(get_num_characters(get_book_text(filepath)))
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+def get_num_characters(book_text):
+    charakter_dic = {}
+    for c in book_text:
+        if c.lower() in charakter_dic:
+            charakter_dic[c.lower()] += 1
+        else:
+            charakter_dic[c.lower()] = 1
+    return charakter_dic
