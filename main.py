bda7489a87565e24bd7b9aca35d176279657d77e
hugermuger <tobias@quasiso.de>
2025-10-02 17:36:33 +0200

    added book choice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_characters, sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,18 +7,22 @@ def get_book_text(filepath):
     return file_content
 
 def main():
-    filepath = "books/frankenstein.txt"
-    num_words = get_num_words(get_book_text(filepath))
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    num_dic = get_num_characters(get_book_text(filepath))
-    sorted_num_dic = sorted_list(num_dic)
-    for i in sorted_num_dic:
-        if i["name"].isalpha() and i["name"] != None:
-            print(f"{i["name"]}: {i["num"]}")
-    print("============= END ===============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+        num_words = get_num_words(get_book_text(filepath))
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        num_dic = get_num_characters(get_book_text(filepath))
+        sorted_num_dic = sorted_list(num_dic)
+        for i in sorted_num_dic:
+            if i["name"].isalpha() and i["name"] != None:
+                print(f"{i["name"]}: {i["num"]}")
+        print("============= END ===============")
 
 main()
